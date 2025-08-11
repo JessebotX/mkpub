886e7d4862f7d4561218c9792311d1ad889a99ca
Jesse <jessebot.git@gmail.com>
2025-08-11 12:28:01 -0700

    refactor: support any type to template funcmap parameters

--- templatefuncs.go
+++ templatefuncs.go
@@ -1,7 +1,13 @@
 package mkpub
 
 import (
+	"errors"
 	"html/template"
+	"strconv"
+)
+
+var (
+	ErrNotFloat = errors.New("arguments must be rational numbers")
 )
 
 var TemplateFuncs = template.FuncMap{
@@ -13,58 +19,102 @@ var TemplateFuncs = template.FuncMap{
 	"dec": dec,
 }
 
-func add(nums ...float64) float64 {
+func convFloat(num any) (float64, error) {
+	numStr, ok := num.(string)
+	if !ok {
+		return -1, ErrNotFloat
+	}
+
+	n, err := strconv.ParseFloat(numStr, 64)
+	if err != nil {
+		return -1, ErrNotFloat
+	}
+
+	return n, nil
+}
+
+func add(nums ...any) (float64, error) {
 	var result float64
-	for i, n := range nums {
+	for i, num := range nums {
+		n, err := convFloat(num)
+		if err != nil {
+			return -1, err
+		}
+
 		if i == 0 {
 			result = n
 			continue
 		}
 		result += n
 	}
-	return result
+	return result, nil
 }
 
-func sub(nums ...float64) float64 {
+func sub(nums ...any) (float64, error) {
 	var result float64
-	for i, n := range nums {
+	for i, num := range nums {
+		n, err := convFloat(num)
+		if err != nil {
+			return -1, err
+		}
+
 		if i == 0 {
 			result = n
 			continue
 		}
 		result -= n
 	}
-	return result
+	return result, nil
 }
 
-func mul(nums ...float64) float64 {
+func mul(nums ...any) (float64, error) {
 	var result float64
-	for i, n := range nums {
+	for i, num := range nums {
+		n, err := convFloat(num)
+		if err != nil {
+			return -1, err
+		}
+
 		if i == 0 {
 			result = n
 			continue
 		}
 		result *= n
 	}
-	return result
+	return result, nil
 }
 
-func div(nums ...float64) float64 {
+func div(nums ...any) (float64, error) {
 	var result float64
-	for i, n := range nums {
+	for i, num := range nums {
+		n, err := convFloat(num)
+		if err != nil {
+			return -1, err
+		}
+
 		if i == 0 {
 			result = n
 			continue
 		}
 		result /= n
 	}
-	return result
+	return result, nil
 }
 
-func inc(n float64) float64 {
-	return n + 1
+func inc(num any) (float64, error) {
+	n, err := convFloat(num)
+	if err != nil {
+		return -1, err
+	}
+
+	return n + 1, nil
 }
 
-func dec(n float64) float64 {
-	return n - 1
+func dec(num any) (float64, error) {
+	n, err := convFloat(num)
+	if err != nil {
+		return -1, err
+	}
+
+	return n - 1, nil
 }
